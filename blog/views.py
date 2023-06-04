dcd6366403ca51c844959d285b96f9a690bfdefe
AleksandrUche <zak.reizer@mail.ru>
2023-06-04 14:08:47 +0300

    added search view

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .forms import EmailPostForm, CommentForm
-from .models import Post, Comment
-from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm, SearchForm
+from .models import Post
+# from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 def post_list(request, tag_slug=None):
@@ -123,3 +124,24 @@ def post_comment(request, post_id):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(
+        request,
+        'blog/post/search.html',
+        {'form': form, 'query': query, 'results': results}
+    )
